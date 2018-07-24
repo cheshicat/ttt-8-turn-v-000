46402b7d826a2b89991d5313a754fad3df5f862f
Allison Howard <alliemhow@gmail.com>
2018-07-24 04:47:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,4 +11,9 @@ def input_to_index(user_input)
 end
 
 def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  end
 end
